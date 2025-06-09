fe480962501a25a49285c6bd8a2f2949ebfd5cc0
ojs83 <ojs831574@gmail.com>
2025-06-10 01:37:32 +0900

    1.heap size정의 및 queue 구조체에 heap 배열 및 mutex 변수 추가

--- qtype.h
+++ qtype.h
@@ -1,6 +1,8 @@
 #ifndef _QTYPE_H  // header guard
 #define _QTYPE_H
-
+// key 값이 최대 10000000이므로 dequeue 없이 enqueue만 있다는 가정하에 최대 사이즈의 배열로 선언
+#define MAX_HEAP_SIZE 10000000
+#include <mutex>
 // ==========이 파일은 수정 가능==========
 
 typedef unsigned int Key;  // 값이 클수록 높은 우선순위
@@ -23,8 +25,12 @@ typedef struct node_t {
     // 필드 추가 가능
 } Node;
 
+//======스레드 safe queue 구현을 위해 연결리스트 대신 heap배열 사용하기로 결정)
 typedef struct {
-    Node* head, tail;
+    Node* head, *tail;
+    Item heap[MAX_HEAP_SIZE];
+    int size;
+    std::mutex lock;
     // 필드 추가 가능
 } Queue;
 
