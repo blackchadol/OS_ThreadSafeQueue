d82c4d5fd0fe985efc600187a960ba98af2551cd
ojs83 <ojs831574@gmail.com>
2025-06-10 01:57:15 +0900

    4. Heap 삽입 후 위로 정렬 및 heap 삭제 후 아래로 정렬하는 heapify_up, heapify_down 함수 구현(MAX heap 기반으로)

--- queue.cpp
+++ queue.cpp
@@ -34,6 +34,43 @@ Node* nclone(Node* node) {
 	return NULL;
 }
 
+void swap(Item& a, Item& b) {
+	Item tmp = a;
+	a = b;
+	b = tmp;
+}
+
+//===key 값이 높을 수록 우선순위가 높기 때문에 MAX_HEAP 기반 자료구조 구현====//
+void heapify_up(Queue* queue, int index) {
+	while (index > 0) {
+		int parent = (index - 1) / 2;
+		if (queue->heap[parent].key >= queue->heap[index].key) break; 
+		swap(queue->heap[parent], queue->heap[index]);
+		index = parent;
+	}
+}
+
+void heapify_down(Queue* queue, int index) {
+	int largest = index;
+	int left = 2 * index + 1;
+	int right = 2 * index + 2;
+
+	// 왼쪽 자식이 더 크면 largest 업데이트
+	if (left < queue->size && queue->heap[left].key > queue->heap[largest].key)
+		largest = left;
+
+	// 오른쪽 자식이 더 크면 largest 업데이트
+	if (right < queue->size && queue->heap[right].key > queue->heap[largest].key)
+		largest = right;
+
+	// largest가 바뀌었으면 교환하고 재귀 호출
+	if (largest != index) {
+		swap(queue->heap[index], queue->heap[largest]);
+		heapify_down(queue, largest);  // 재귀 호출
+	}
+}
+
+
 
 Reply enqueue(Queue* queue, Item item) {
 	Reply reply = { false, NULL };
